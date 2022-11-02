d69b1ff520add782948e31d361c8c732c08f1e61
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-11-02 23:54:02 +0000

    feat: init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import expect from 'expect'
+import { toMatchImageSnapshot } from 'expect-mocha-image-snapshot'
+import toMatchSnapshot from 'expect-mocha-snapshot'
+
+expect.extend({ toMatchSnapshot })
+expect.extend({ toMatchImageSnapshot })
+global.expect = expect
